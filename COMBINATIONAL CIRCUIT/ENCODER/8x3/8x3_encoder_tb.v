3bd160a5836296345b5a666a7e67e09a6b84b28b
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-02-06 17:00:08 +0530

    Update 8x3_encoder_tb.v

--- COMBINATIONAL CIRCUIT/ENCODER/8x3/8x3_encoder_tb.v
+++ COMBINATIONAL CIRCUIT/ENCODER/8x3/8x3_encoder_tb.v
@@ -1,20 +1,28 @@
 module encoder_8x3_tb();
   reg [7:0]a;
   wire [3:0]b;
-  encoder_8x3 dut (a,b);
+  reg en;
+  encoder_8x3 dut (a,en,b);
   initial begin
-    $monitor("time=%0t,a=%b,b=%b",$time,a,b);
-       a=8'b00000000;
-    #5;a=8'b00011100;
-    #5;a=8'b00001000;
-    #5;a=8'b00010000;
-    #5;a=8'b00100000;
-    #5;a=8'b01000000;
-    #5;a=8'b10000000;
+    $monitor("time=%0t,en=%b,a=%b,b=%b",$time,en,a,b);
+       en=1;a=8'b00000000;
+    #5;en=1;a=8'b00011100;
+    #5;en=1;a=8'b00001000;
+    #5;en=1;a=8'b00010000;
+    #5;en=1;a=8'b00100000;
+    #5;en=1;a=8'b01000000;
+    #5;en=1;a=8'b10000000;
+    #5;en=0;a=8'b00000000;
+    #5;en=0;a=8'b00011100;
+    #5;en=0;a=8'b00001000;
+    #5;en=0;a=8'b00010000;
+    #5;en=0;a=8'b00100000;
+    #5;en=0;a=8'b01000000;
+    #5;en=0;a=8'b10000000;
   end
   initial begin
     $dumpfile("TVK.vcd");
     $dumpvars(0,encoder_8x3_tb);
-    #50 $finish;
+    #200 $finish;
   end 
 endmodule
