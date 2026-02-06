842e8a548a9c912d07eaea2eb7e3b53a4c68a664
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-02-06 16:54:34 +0530

    8x3_encoder.v

--- COMBINATIONAL CIRCUIT/ENCODER/8x3/8x3_encoder.v
+++ COMBINATIONAL CIRCUIT/ENCODER/8x3/8x3_encoder.v
@@ -1,17 +1,23 @@
-module encoder_8x3(a,b);
+module encoder_8x3(a,en,b);
   input [7:0]a;
-  output reg [3:0]b;
-  always@(*)begin
-    case(a)
-      8'b00000001:b=3'b000;
-      8'b00000010:b=3'b001;
-      8'b00000100:b=3'b010;
-      8'b00001000:b=3'b011;
-      8'b00010000:b=3'b100;
-      8'b00100000:b=3'b101;
-      8'b01000000:b=3'b110;
-      8'b10000000:b=3'b111;
-      default b=3'bxxx;
-    endcase
-  end
+  input en;
+  output reg [2:0]b;
+    always@(*)begin
+      if (en==1)begin
+        case(a)
+          8'b00000001:b=3'b000;
+          8'b00000010:b=3'b001;
+          8'b00000100:b=3'b010;
+          8'b00001000:b=3'b011;
+          8'b00010000:b=3'b100;
+          8'b00100000:b=3'b101;
+          8'b01000000:b=3'b110;
+          8'b10000000:b=3'b111;
+            default :b=3'bxxx;
+        endcase
+      end
+      else begin
+         b=3'b000;
+      end
+    end
 endmodule
