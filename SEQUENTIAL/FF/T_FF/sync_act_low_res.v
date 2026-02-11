393072bd289d5358ec94e09308d2f87e26f74f8a
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-02-11 12:01:31 +0530

    Create sync_act_low_res.v

---
+++ SEQUENTIAL/FF/T_FF/sync_act_low_res.v
@@ -0,0 +1,13 @@
+module tff(t,res,clk,q);
+  input t,res,clk;
+  output reg q;
+  always @ (posedge clk)begin
+    if (res)
+      case(t)
+        0:q<=q;
+        1:q<=~q;
+      endcase
+    else
+      q<=0;
+  end 
+endmodule
