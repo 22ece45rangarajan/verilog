6217f29d4f76c03f1909e345bcb34efceee73f84
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-02-11 12:04:04 +0530

    Create sync_act_high_res.v

---
+++ SEQUENTIAL/FF/T_FF/sync_act_high_res.v
@@ -0,0 +1,13 @@
+module tff(t,res,clk,q);
+  input t,res,clk;
+  output reg q;
+  always @ (posedge clk)begin
+    if (res)
+      q<=0;
+    else
+      case(t)
+        0:q<=q;
+        1:q<=~q;
+      endcase
+  end 
+endmodule
