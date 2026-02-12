959a983bcac22951caefd761bc7ac50f3287de3c
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-02-12 10:38:37 +0530

    Create sipo_right_shift.v

---
+++ SEQUENTIAL/FF/REGISTER/SIPO/sipo_right_shift.v
@@ -0,0 +1,16 @@
+module sipo(d,res,clk,y);
+  input d;
+  input res,clk;
+  output reg[3:0]y;
+  reg [3:0]q;
+  always@(posedge clk)begin
+    if(res)begin
+      q<=4'b0000;
+    end
+    else begin
+      q<=q>>1;
+      q[3]<=d;
+      y<=q;
+    end
+   end
+endmodule
