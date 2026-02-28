01add7620892d80e466881b1ed08f7fdcd7fe726
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-02-28 18:42:34 +0530

    Update full_using_half.v

--- SRTUCT_ASSIGNMENT/full_using_half.v
+++ SRTUCT_ASSIGNMENT/full_using_half.v
@@ -10,3 +10,22 @@ module FA(input A,B,C, output Sum,COUT);
   ha HA2(.a(S1),.b(C),.SUM(Sum),.CARRY(C2));
   assign COUT=C1|C2;
 endmodule
+//test_bench
+module FA2HA();
+  reg a,b,c;;
+  reg sum,carry;
+  FA dut(a,b,c,sum,carry);
+  initial begin
+    $monitor ("time=0%t,a=%b,b=%b,c=%b | sum=%b,carry=%b",$time,a,b,c,sum,carry);
+    #10;a=0;b=0;c=1;
+    #10;a=0;b=1;c=1;
+    #10;a=1;b=1;c=1;
+    #10;a=0;b=0;c=0;
+    #10;a=0;b=1;c=0;
+    #10;a=1;b=1;c=0;
+  end 
+  initial begin
+    $dumpfile("BLEACH.vcd";
+              $dumpvarse("0,FA2HA);
+                         #100 $finish:
+endmodule
