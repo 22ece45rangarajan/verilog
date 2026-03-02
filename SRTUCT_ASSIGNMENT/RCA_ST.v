d6c607db9ca2bd2ee3b3cb981926db0554acaaa7
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-03-02 10:15:00 +0530

    Update RCA_ST.v

--- SRTUCT_ASSIGNMENT/RCA_ST.v
+++ SRTUCT_ASSIGNMENT/RCA_ST.v
@@ -1,3 +1,5 @@
+//Design a 4-bit Ripple Carry Adder using structural modeling
+
 module Fulladder(input a,b,Cin,output s,cout);
   assign s=a^b^Cin;
   assign cout=(a&b)|(b&Cin)|(a&Cin);
@@ -16,6 +18,35 @@ endmodule
 
 //test_bench
 
+
+module RIPPLE_CARRY_ADDER_TB();
+ reg[3:0] A, B;
+ reg Cin;
+ wire[3:0]SUM;
+ wire CARRY; 
+ RCA dut(A,B,Cin,SUM,CARRY);
+  initial begin 
+    $monitor("time=%0t,A=%d,B=%d,Cin=%d | SUM=%d,CARRY=%d",$time,A,B,Cin,SUM,CARRY);
+    
+    #10;A=4'b0000;B=4'b0000;Cin=0;
+    #10;A=4'b0001;B=4'b0001;Cin=0;
+    #10;A=4'b0010;B=4'b0010;Cin=0;
+    #10;A=4'b0011;B=4'b0011;Cin=0;
+    #10;A=4'b0100;B=4'b0100;Cin=0;
+    #10;A=4'b1101;B=4'b1101;Cin=0;
+    #10;A=4'b0000;B=4'b0000;Cin=1;
+    #10;A=4'b0001;B=4'b0001;Cin=1;
+    #10;A=4'b0010;B=4'b0010;Cin=1;
+    #10;A=4'b0011;B=4'b0011;Cin=1;
+    #10;A=4'b0100;B=4'b0100;Cin=1;
+    #10;A=4'b1101;B=4'b1101;Cin=1;
+  end
+  initial begin
+    $dumpfile("TVK.vcd");
+    $dumpvars(0,RIPPLE_CARRY_ADDER_TB);
+    #150 $finish;
+  end 
+endmodule
 
 
 
