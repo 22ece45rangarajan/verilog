2de7078e9be35d7fd462cc906b970a7621d09d05
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-03-06 15:17:11 +0530

    Create freq_div_duty_75.v

---
+++ SEQUENTIAL/FREQ_VAGUPPAN/freq_div_duty_75.v
@@ -0,0 +1,28 @@
+module freq_divider_6(clk_in,res,clk_out);
+  input clk_in,res;
+  output   clk_out;
+  reg [2:0]counter;
+  reg pos_count,neg_count;
+  wire out;
+  always @(posedge clk_in)begin
+    if(res)begin
+      counter<=0;
+      pos_count<=0;
+      neg_count<=0;
+    end
+    else begin
+      if(counter==3'd5)
+        counter<=0;
+      else
+        counter<=counter+1;
+    end
+  end
+  always@(posedge clk_in)begin
+    pos_count<=(counter==0);
+  end
+  always@(negedge clk_in)begin
+    neg_count<=(pos_count);
+  end
+  assign out=pos_count&neg_count;
+  assign clk_out=~(out);
+endmodule
