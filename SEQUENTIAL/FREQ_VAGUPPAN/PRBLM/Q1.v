b4fa744ed3c8bcc572cbf191ee05f043bcc14765
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-03-06 17:07:00 +0530

    Update and rename SEQUENTIAL/FREQ_VAGUPPAN/question_online.v to SEQUENTIAL/FREQ_VAGUPPAN/PRBLM/Q1.v

--- SEQUENTIAL/FREQ_VAGUPPAN/PRBLM/Q1.v
+++ SEQUENTIAL/FREQ_VAGUPPAN/PRBLM/Q1.v
@@ -29,21 +29,6 @@ module freq_divider_10(clk_in,res,clk_out);
   end
 endmodule
 
-//___test_bench___//
 
-module freq_divider_10_tb();
-  reg clk_in,res;
-  wire clk_out;
-  freq_divider_10 dut(.*);
-  always #5 clk_in=~clk_in;
-  initial begin
-    $monitor("time=%0t||clk_in=%b||res=%b||clk_out=%b",$time,clk_in,res,clk_out);
-    res=1;clk_in=0;
- #10 res=0;
-  end
-  initial begin
-    $dumpfile("tvk.vcd");
-    $dumpvars(0,freq_divider_10_tb);
-    #1500 $finish;
-  end
-endmodule
+
+
